ff82c8d68009c1a21b0d60c5f97d357fa7717842
Jake Musich <makejusich@gmail.com>
2017-05-06 00:57:05 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def match(jumble)
+    result = []
+    brokenname = @name.split("")
+    jumble.each do |w|
+      if w.split("").sort == brokenname.sort
+        result << w
+      end
+    end
+    result 
+  end
+
+
+end
